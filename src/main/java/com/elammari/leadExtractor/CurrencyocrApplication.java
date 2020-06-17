efa577342fce9835bf0eec0f894a926e2246cabb
Soufiane ELAMMARI <elammarisoufiane@gmail.com>
2020-06-17 22:39:15 +0100

    First commit

---
+++ src/main/java/com/elammari/leadExtractor/CurrencyocrApplication.java
@@ -0,0 +1,24 @@
+package com.elammari.leadExtractor;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CurrencyocrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyocrApplication.class, args);
+	}
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            String value = ImageParse.parseImage("c.jpg");
+            System.out.println(">>>>>>>" + value);
+
+        };
+    }
+}
